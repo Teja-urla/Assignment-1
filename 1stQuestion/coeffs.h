14463588842d8ba4e9fed531d743b29512731109
Teja-urla <102712609+Teja-urla@users.noreply.github.com>
2022-06-27 16:34:02 +0530

    Create coeffs.h

---
+++ 1stQuestion/coeffs.h
@@ -0,0 +1,16 @@
+void uniform(char *str, int len);
+
+void uniform(char *str, int len)
+{
+int i;
+FILE *fp;
+
+fp = fopen(str,"w");
+//Generate numbers
+for (i = 0; i < len; i++)
+{
+fprintf(fp,"%lf\n",(double)rand()/RAND_MAX);
+}
+fclose(fp);
+
+}
