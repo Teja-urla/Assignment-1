9790209a0be16c3c0324244c17d0d11d0424a566
Teja-urla <102712609+Teja-urla@users.noreply.github.com>
2022-07-11 12:31:20 +0530

    Create 5.2.c

---
+++ 5thSection/5.2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "source.h"
+int  main(void) //main function begins
+{
+ 
+combo("5.2.dat", "gau.dat","equi.dat",1000000);
+
+return 0;
+}
